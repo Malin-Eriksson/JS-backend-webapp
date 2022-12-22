c514a3fc9be86b7d20648a35e85835c7f049f619
Malin-Eriksson <sangsta.malin@gmail.com>
2022-12-22 17:52:02 +0100

    update backend working

--- src/sections/UpdateProductSection.tsx
+++ src/sections/UpdateProductSection.tsx
@@ -1,31 +1,32 @@
 import React, { useState } from 'react'
-import { idText } from 'typescript'
 import FormNotification from '../components/FormNotification'
-import { currencyFormatter } from '../components/utilities/currencyFormatter'
 import { ProductItem } from '../models/ProductModels'
 
+
 interface UpdateProductType {
-item: ProductItem
+  item: ProductItem
 }
 
-const UpdateProductSection: React.FC<UpdateProductType> = ({item}) => {
+
+
+const UpdateProductSection: React.FC<UpdateProductType> = () => {
   const [updateProduct, setUpdateProduct] = useState<boolean>(false)
   const [updateFailed, setUpdateFailed] = useState<boolean>(false)
 
 
+    const handleUpdateProduct = async (e: React.FormEvent) => {
+      e.preventDefault()
+      setUpdateProduct(false)
+      setUpdateFailed(false)
 
-  
-  const handleUpdateProduct = async (_id: string) => {
-    setUpdateProduct(false)
-    setUpdateFailed(false)
+
 
     const result = await fetch(`http://localhost:5000/api/products/update`, {
-      method: 'post',
+      method: 'put',
       headers: {
         'Content-type': 'application/json',
         'authorization': `Bearer ${localStorage.getItem('accessToken')}`
       },
-      body: JSON.stringify({item})
     })
 
     if (result.status === 200) {
@@ -46,26 +47,26 @@ const UpdateProductSection: React.FC<UpdateProductType> = ({item}) => {
           {updateProduct ? (<FormNotification notificationType='success' title='Product updated' text=''/>) : (<></>)}
           {updateFailed ? (<FormNotification notificationType='danger' title='Something went wrong!' text="We couln't update the product - please try again later!"/>) : (<></>)}
 
-          <form>
+          <form onSubmit={handleUpdateProduct} noValidate>
             <div>
               <img src={item.imageName} />    
             </div>
             <h3>Product name</h3>
-            <input id="name" value={item.name} type='text' placeholder={item.name}/>
+            <input id="name" value={item.name} onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={item.name}/>
             <h3>Description</h3>
-            <input id="description" type='text' placeholder={item.description}/> 
+            <input id="description" value={item.description} onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={item.description}/> 
             <h3>Price</h3>
-            <input id="price" type='text' placeholder={String(item.price)}/> 
+            <input id="price" value={item.price} onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={String(item.price)}/> 
             <h3>Category</h3>
-            <input id="category" type='text' placeholder={item.category}/>  
+            <input id="category" value={item.category}  onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={item.category}/>  
             <h3>Tag</h3>
-            <input id="tag" type='text' placeholder={item.tag}/> 
+            <input id="tag" value={item.tag} onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={item.tag}/> 
             <h3>Image name</h3>
-            <input id="imageName" type='text' placeholder={item.imageName}/> 
+            <input id="imageName" value={item.imageName} onChange={(e) => handleUpdateProduct(e)} type='text' placeholder={item.imageName}/> 
             <h3>Rating, 1-5</h3>
-            <input id="rating" type='number' placeholder={String(item.rating)}/> 
+            <input id="rating" value={item.rating} onChange={(e) => handleUpdateProduct(e)} type='number' placeholder={String(item.rating)} /> 
 
-            <button onClick={() => handleUpdateProduct(item.articleNumber)}>UPDATE PRODUCT</button>
+            <button type='submit'>UPDATE PRODUCT</button>
 
           </form>
         </div>
